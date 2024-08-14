e63418529ef9c0fc70e4526c83be27e90caae7f2
syedhuzaif199 <syedhuzaif199@gmail.com>
2024-08-14 14:59:38 +0530

    Theme change

--- hall-of-fame/sketch.js
+++ hall-of-fame/sketch.js
@@ -15,7 +15,7 @@ function setup() {
   for (let i = 0; i < n; i++) {
     xs.push(random(20, windowWidth - 20));
     ys.push(random(20, windowHeight - 20));
-    radii.push(random(10, 20));
+    radii.push(random(5, 7));
     let ang = random(0, 2 * PI);
     velocities.push([cos(ang), sin(ang)]);
   }
@@ -23,13 +23,13 @@ function setup() {
 
 function draw() {
   clear();
-  let thresh = 16000;
+  let thresh = 10000;
   background(0, 0, 0, 0);
   for (let i = 0; i < n - 1; i++) {
     for (let j = i + 1; j < n; j++) {
       let dis = (xs[i] - xs[j]) ** 2 + (ys[i] - ys[j]) ** 2;
       if (dis < thresh) {
-        let col = color(255, 255, 255, 255 * (1 - (1.2 * dis) / thresh));
+        let col = color(255, 255, 255, 220 * (1 - dis / thresh));
         stroke(col);
         line(xs[i], ys[i], xs[j], ys[j]);
       }
@@ -45,11 +45,11 @@ function draw() {
       velocities[i][1] *= -1;
     }
     stroke(0, 0, 0, 0);
-    fill(255);
+    fill(255 + (5 - radii[i]) * 60);
     circle(xs[i], ys[i], radii[i]);
   }
 
-  background(0, 0, 0, 150);
+  // background(0, 0, 0, 150);
 }
 
 function windowResized() {
